f256075519723121cb9a65f068e90baf878357aa
Lukáš Divíšek <76947123+Bruzdden@users.noreply.github.com>
2021-03-08 12:32:33 +0100

    Create tahak.h
    Toto je můj tahák na html

---
+++ tahak.h
@@ -0,0 +1,88 @@
+<p> Hello im new here </p>
+	<head> 
+		<title> jmeno stranky </title>
+	</head>
+	<body bgcolor="black"> <!-- zmeni barvu pozadi-->
+		h1><span>Contact me</span></h1>
+        	<form>
+            		<input name="name" type="text" /><br/>
+            		<input name="email" type="email" /><br/>
+            		<textarea name="message"></textarea>
+            		<input type="submit" value="SEND" class="submit" />
+        	</form>
+		<ol> 
+			<li> Red <li/>
+			<li> Blue <li/>
+			<li> Green <li/>
+		<form>
+            		<input type="text" name="username"/>
+            		<input type="password" name="password"/>
+        	</form>    
+		<form>
+            		<input type="checkbox" name="gender" value="1"/> muz <!-- checkbox je zaskrtavaci a da se dat pryc -->
+			<input type="submit" value="submit"/>
+            		<input type="radio" name="gender" value="2"/> zena <!-- radio zaklikne a nejde vratit -->
+        	</form>
+		<form></form> <!-- strebava informace -->
+		<table border ="3"> <!-- border na ohraniceni sloupce a cislo jak je to velky -->
+			<tr>
+				<td colspan ="cislo">mezi se vklada text</td> <!-- colspan zvetseni bunky o cislo -->
+				<td bgcolor ="green"></td> <!-- bgcolor vyplni bunku barvou -->
+			</tr>
+		</table>
+		<ol/>
+		<p align="center"> this is a line of text.<p/> <!-- align = umisti kde bude text nebo neco jineho -->
+		<h2> Nadpis 2 </h2>
+		<hr width="%50px" /> <!-- umozni zvetseni cary -->
+		<!-- tohle je komentaar --> 
+		<p><b> Tento text je napsany velce</b></p>
+	</body>
+<tag>
+
+.html
+METHOD="GET" data budou viditelna
+METHOD="POST" treba na hesla 
+submit je potvrzeni
+<ol> cisla v listu
+<ul> tecky v listu
+
+p je element pro psani paragrafu
+Use the <a href="odkaz" target="_blank"> <!-- blank otevre odkaz na novy strance --> odkaz </a>
+Use the <hr/> aby to udelalo rovnou caru mezi texty
+Use the <!-- koment  --> aby udelal kometar
+Use the <br /> abys pridal nevy paragraf - udela to vetsi mezeru jako break, da to do noveho radku
+Use the <img/> aby se mohl vlozit obrazek <img src="image.jpg" alt="" />
+Use the <a href="odkaz" > klikni sem na otevreni odkazu <a/> umozni aby si mohl kliknout treba na odkaz
+
+
+		Atributy: align= - umoznuje kde bude text polozeny
+			  action= - udela akci
+			  width= - tloustka, sirka
+			  height= - vyska
+			  border="" - udela to ohraniceni kolem fotky
+			  target="" - umozni kde nebo jak se odkaz otevre
+			  class ="" -
+			  div je block level element
+			  href = - url stranky
+		elementy:
+			APPLET - embedded Java applet
+			IFRAME- Inline frame
+			INS - inserted text
+			MAP - image map
+			OBJECT - embedded object
+			SCRIPT - script within an HTML document
+
+
+<p><b> Tisteny text </b></p>
+<p><big> velky text </big></p>
+<p><i> italsky text </i></p>
+<p><small> maly text </small></p>
+<p><strong> silny text </strong></p>
+<p><sub> subscripted text </sub></p>
+<p><sup> superscripted text </sup></p>
+<p><ins> podtrzeny text </ins></p>
+<p><del> preskrtnuty text </del></p>
+
+Tables are defined by using the <table> tag.
+Tables are divided into table rows with the <tr> tag.
+Table rows are divided into table columns (table data) with the <td> tag. sloupce
